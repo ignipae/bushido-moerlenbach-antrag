db3d2cf0be030ab587c6ce1a338eb5bd7a6cc5bd
Julian Breuksch <julian.breuksch@sap.com>
2019-10-09 14:29:36 +0200

    fix

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/bushido-moerlenbach-antrag'));
-});
+app.use(express.static(__dirname + '/dist/bushido-moerlenbach-antrag'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, err => {
+    if (err) {
+        throw err;
+    }
+    console.log('API Up and running on port ' + port);
+});
