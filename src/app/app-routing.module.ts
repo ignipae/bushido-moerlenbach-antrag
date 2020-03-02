4d9cef41519754700ff5c92971fca590639cb70b
Julian Breuksch <julian.breuksch@sap.com>
2020-03-02 16:27:59 +0100

    use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,25 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {AntragComponent} from './antrag/antrag.component';
-import {KuendigungComponent} from './kuendigung/kuendigung.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AntragComponent } from "./antrag/antrag.component";
+import { KuendigungComponent } from "./kuendigung/kuendigung.component";
 
-const routes : Routes = [
+const routes: Routes = [
   {
     path: "",
     component: AntragComponent
-  }, {
+  },
+  {
     path: "antrag",
     component: AntragComponent
-  }, {
+  },
+  {
     path: "kuendigung",
     component: KuendigungComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
 
   exports: [RouterModule]
 })
